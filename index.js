581d79541e6bbe966cc61015605886cd3e628bd4
WellsJhones <wellsjhones@gmail.com>
2024-01-22 23:39:00 -0300

    add comments and fix function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let output = document.querySelector(".text__output");
 let entrada = document.querySelector(".text__input");
+//funcao para capturar o valor do input
 function codify() {
   output.value = "";
   let text = document.querySelector(".text__input").value;
@@ -23,10 +24,11 @@ function codify() {
     } else word += text[i];
   }
   //   console.log(word);
-
+  //devolve o valor para o output e reseta o campo input
   output.value = word;
   entrada.value = "";
 }
+//funcao para decodificar o texto do input
 function decodify() {
   let text = document.querySelector(".text__input").value;
   let word = "";
@@ -37,9 +39,10 @@ function decodify() {
     .replace(/ober/g, "o")
     .replace(/ufat/g, "u");
   console.log(word);
-  document.querySelector(".text__output").value = word;
+  output.value = word;
   entrada.value = "";
 }
+//funcao para copiar o texto do output para a area de transferencia
 function copy() {
   let text = document.querySelector(".text__output").value;
   navigator.clipboard.writeText(text);
